6055df90cea15409756439d6bfd3dd1f7d11b366
Yiyuan Ma <yim096@ucsd.edu>
2018-01-30 08:09:44 -0800

    Initial Commit

---
+++ flix_demo_1Tests/flix_demo_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  flix_demo_1Tests.swift
+//  flix_demo_1Tests
+//
+//  Created by 马懿元 on 1/30/18.
+//  Copyright © 2018 Sparrow. All rights reserved.
+//
+
+import XCTest
+@testable import flix_demo_1
+
+class flix_demo_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
